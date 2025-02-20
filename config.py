e97f0901e5701d070a9e07f9ab3ee127f44cb0db
insane_catt <129239662+insane-catt@users.noreply.github.com>
2025-02-20 10:03:39 +0900

    config.pyに英語の説明を追加、languages.pyを追加

--- config.py
+++ config.py
@@ -1,9 +1,31 @@
-# 以下は簡単な設定欄です
+# 以下は簡単な設定欄です。Below is a simple configuration field.
+# 文字列はダブルクォーテーションで "このように" 囲ってください。Please enclose the string in double quotes "like this".
 
-TOKEN = "TOKEN"  # Discord botのトークンを入力してください
-SERVER_PATH = "minecraft/java/paper"  # Minecraft実行ファイルのあるフォルダのホームディレクトリからのパスを入力してください
-HOME_DIRECTORY = "/home/pi"  # ホームディレクトリのパスを入力してください
-JAR_FILE = "paper.jar"  # 使用しているMinecraftサーバー実行ファイルの名前を入力してください
-SCREEN_NAME = "papermc"  # 使用するscreenセッションの名前を入力してください
-MAX_RAM = "5"  #使用する最大メモリを設定してください。デフォルトは5
-MIN_RAM = "2"  #使用する最小メモリを設定してください。デフォルトは2
+# 使用する言語を選択してください。日本語は"ja"、英語は"en"を入力してください。
+# Please select the language you want to use. Enter "ja" for Japanese and "en" for English.
+LANG = "ja"
+
+# Discord botのトークンを入力してください。
+# Please enter your Discord bot token.
+TOKEN = "TOKEN"
+
+# Minecraft実行ファイルのあるフォルダのホームディレクトリからのパスを入力してください。
+# Please enter the path from the home directory of the folder where the Minecraft executable file is located.
+SERVER_PATH = "minecraft/java/paper"
+
+# ホームディレクトリのパスを入力してください。
+# Please enter the path to the home directory.
+HOME_DIRECTORY = "/home/pi"
+
+# 使用しているMinecraftサーバー実行ファイルの名前を入力してください。
+# Please enter the name of the Minecraft server executable file you are using.
+JAR_FILE = "paper.jar"
+
+# 使用するscreenセッションの名前を入力してください。
+# Please enter the name of the screen session you want to use.
+SCREEN_NAME = "papermc"
+
+# 使用する最大メモリと最小メモリを設定してください。
+# Please set the maximum and minimum memory you want to use.
+MAX_RAM = "5"
+MIN_RAM = "2"
